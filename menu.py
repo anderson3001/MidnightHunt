15c644bba3098150eb7a84f86330f62b07c4e56f
Anderson Souza Gomes <derson3001@gmail.com>
2025-01-21 14:50:41 -0300

    Atualizando Menu

--- menu.py
+++ menu.py
@@ -1,6 +1,6 @@
 from PPlay.window import *
 from PPlay.gameimage import *
-
+import importlib
 
 janela = Window(1920, 1080)
 cenario = GameImage("Sprites/MH_Menu_fundo.png")
@@ -14,7 +14,6 @@ sair = GameImage("Sprites/MH_botao_sair.png")
 sair.x = janela.width/2 - sair.width/2
 sair.y = 210 + 400
 mouse = Window.get_mouse()
-
 while True:
     janela.set_title("MidnightHunt")
     cenario.draw()
@@ -22,8 +21,9 @@ while True:
     play.draw()
     sair.draw()
     if mouse.is_over_area([play.x, play.y],[play.x + play.width, play.y + play.height]) and mouse.is_button_pressed(1):
-        #começa o jogo
+        import game
+        importlib.reload(game)
     if mouse.is_over_area([sair.x, sair.y],[sair.x + sair.width, sair.y + sair.height]) and mouse.is_button_pressed(1):
         janela.close()
 
-        janela.update()
+    janela.update()
